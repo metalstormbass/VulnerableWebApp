c48e3c2e92b5faf0938eedd4df8e853dce2937f2
Michael Braun <michaelbraunbass@gmail.com>
2020-08-24 07:17:12 -0600

    Update startup.sh

--- VulnerableWebApp/startup.sh
+++ VulnerableWebApp/startup.sh
@@ -1,2 +1,4 @@
 pip install django
-gunicorn /home/site/wwwroot/VulnerableWebApp/VulnerableWebApp.wsgi --bind=0.0.0.0:80
+gunicorn --workers 8 --threads 4 --timeout 60 --access-logfile \
+    '-' --error-logfile '-' --bind=0.0.0.0:80  -k uvicorn.workers.UvicornWorker \
+     --chdir=/home/site/wwwroot/VulnerableAzure VulnerableWebApp.wsgi
