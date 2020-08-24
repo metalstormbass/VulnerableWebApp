9f4ef44d41a9ad1385b88c39c40caf8a03e2dab1
Michael Braun <michaelbraunbass@gmail.com>
2020-08-24 10:55:06 -0600

    Update startup.sh

--- VulnerableWebApp/startup.sh
+++ VulnerableWebApp/startup.sh
@@ -1,3 +1,3 @@
-pip install django
+pip install -r requirements.txt
 gunicorn --workers 8 --threads 4 --timeout 60 --access-logfile \
     '-' --error-logfile '-' --bind=0.0.0.0:8000 --chdir=/home/site/wwwroot/VulnerableWebApp/ VulnerableWebApp.wsgi
