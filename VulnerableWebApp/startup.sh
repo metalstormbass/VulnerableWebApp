0b345bd48720bbdb1cfaf201317ae83456714861
Michael Braun <michaelbraunbass@gmail.com>
2020-08-24 12:28:14 -0600

    Update startup.sh

--- VulnerableWebApp/startup.sh
+++ VulnerableWebApp/startup.sh
@@ -1,3 +1,7 @@
-apt install python3-pip -y
-pip3 install -r /home/site/wwwroot/VulnerableWebApp/requirements.txt
+until apt install python3-pip -y;do
+sleep 1
+done
+until pip3 install -r /home/site/wwwroot/VulnerableWebApp/requirements.txt;do
+sleep 1
+done
 gunicorn --bind=0.0.0.0:8000 --chdir=/home/site/wwwroot/VulnerableWebApp/ VulnerableWebApp.wsgi
